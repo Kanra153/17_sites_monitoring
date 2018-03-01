df7e6d494f2135d3045671612a962bbb3c4a409b
Kanra153 <mad_hatter153@bk.ru>
2018-03-01 19:19:28 +0300

    first commit

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,41 @@
+import sys
+import os
+import whois
+import datetime
+import requests
+
+
 def load_urls4check(path):
-    pass
+    if not os.path.exists(file_path):
+        return None
+    with open(path, 'r', encoding='utf-8') as file_handler:
+        urls = file_handler.read().split()
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    r = requests.get(url)
+    return(r.status_code)
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+        try:
+            return(whois.whois(url)['expiration_date'][0])
+        except  TypeError:
+            return(whois.whois(url)['expiration_date'])
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        sys.exit('File is not found!')
+    path = sys.argv[1]
+    urls = load_urls4check(path)
+    now = datetime.datetime.now()
+    for url in urls:
+        print(url)
+        print('The server response code is: ', 
+              is_server_respond_with_200(url))
+        days_until_expiration = get_domain_expiration_date(url)-now
+        print('Days until expiration: ',
+              days_until_expiration.days, '\n')
+        
